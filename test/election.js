dcf35a9a58d7fdee1e78aa51f64c20096f8ebcf6
soumya0803 <soumyachopra@gmail.com>
2018-10-24 09:44:03 +0530

    update test

--- test/election.js
+++ test/election.js
@@ -1,12 +1,30 @@
 var Election = artifacts.require("./Election.sol");//abstraction to interact with contract
 contract("Election",function(accounts){ //declare our contract,this injects all accounts that exist in our deleloment environment
+  
+    var electionInstance;// to save instance, so that it has scope inside this entire test
 
 //test initialized with correct no. of candidates
    it("Initializes with two candidates", function(){   //provided by mocha testing truffleframework
-          return Election.deployed().then(function(instance){
+          return Election.deployed().then(function(instance){ // asynchronous call, promise chain
        return instance.candidatesCount();
      }).then(function(count){
        assert.equal(count, 2); //assert provided by chai
      });
    });
+   it("it initializes the candidates with the correct values", function() {
+  return Election.deployed().then(function(instance) {// gives copy of deployed contract
+    electionInstance = instance;
+    return electionInstance.candidates(1); // call candidates function which was in election.sol 
+  }).then(function(candidate) {
+    assert.equal(candidate[0], 1, "contains the correct id");
+    assert.equal(candidate[1], "Candidate 1", "contains the correct name");
+    assert.equal(candidate[2], 0, "contains the correct votes count");
+    return electionInstance.candidates(2);
+  }).then(function(candidate) {
+    assert.equal(candidate[0], 2, "contains the correct id");
+    assert.equal(candidate[1], "Candidate 2", "contains the correct name");
+    assert.equal(candidate[2], 0, "contains the correct votes count");
+  });
 });
+});
+
